bdc4ca8ba8c1c501263075b00b89e7e7b975b0fc
Emmy Emme <masteremmy.js@gmail.com>
2018-11-18 22:20:42 -0500

    i love you and am lost without you, spring initializr

---
+++ src/main/java/com/yetopia/javaspringtest/JavaspringtestApplication.java
@@ -0,0 +1,12 @@
+package com.yetopia.javaspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringtestApplication.class, args);
+	}
+}
